8bae0732c7ce706457595a3f946524dfafdf0598
Eflex <eflexmedia1@gmail.com>
2024-07-31 20:53:24 +0100

    L

--- src/Updateuser/Oldfile/Updateuser.jsx
+++ src/Updateuser/Oldfile/Updateuser.jsx
@@ -18,7 +18,7 @@ const UpdateWallet = ({Display}) => {
   // const [sessionTime, setSessionTime] = useState()
   
 
-    const url = `https://jollybackend.onrender.com/api/alluserdata`
+    const url = `https://jolly-back-end.vercel.app/api/alluserdata`
   
     useEffect(()=>{
       axios.get(url)
@@ -71,7 +71,7 @@ const UpdateWallet = ({Display}) => {
       // }
 
       const deleteprofile = (id) => {
-        const url = `https://jollybackend.onrender.com/api/userdata/${id}`
+        const url = `https://jolly-back-end.vercel.app/api/userdata/${id}`
         axios.delete(url)
         .then(res => {
           console.log(res)
@@ -89,7 +89,7 @@ const UpdateWallet = ({Display}) => {
 
       const updatedata = {accountBalance, bonus,  ref, totalDeposit, totalWithdrawal};
       const updateprofile = (id) => {
-        const updateuserurl = `https://jollybackend.onrender.com/api/userdata/${id}`
+        const updateuserurl = `https://jolly-back-end.vercel.app/api/userdata/${id}`
           console.log(updateuserurl)
         axios.patch(updateuserurl, updatedata)
         .then(res => {
